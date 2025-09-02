2e7e740e0967b30a29f783b0c96e91c8f31e83c3
Chetanya2001 <mathurchetanya1@gmail.com>
2025-09-02 19:44:42 +0530

    userProfile done

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const userRoutes = require("./user.routes");
+const userProfileRoutes = require("./userProfile.routes");
 
 router.use("/users", userRoutes);
+router.use("/user-profile", userProfileRoutes);
 module.exports = router;
