be2e0a3b0ae624b9467bf1113b5807c160e5250c
Chetanya2001 <mathurchetanya1@gmail.com>
2025-09-21 17:26:38 +0530

    new ymal file

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
-  port: 3300,
+  port: 3306,
   pool: dbConfig.pool,
 });
 
