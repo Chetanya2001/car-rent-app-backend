40777db4ba608e6ca89fdcbc821d3f2f25006722
Chetanya2001 <mathurchetanya1@gmail.com>
2026-02-26 16:24:26 +0530

    upload

--- routes/service.routes.js
+++ routes/service.routes.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
 const controller = require("../controller/car-service.controller");
 const { verifyToken } = require("../middleware/authmiddleware");
+const multer = require("multer");
+
+// ========= Multer setup =========
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 router.post("/book", verifyToken, controller.createBooking);
 router.get("/:id/checklist", controller.getChecklist);
