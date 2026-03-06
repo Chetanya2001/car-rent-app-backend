94f37d59cf3ff0f7bd96a6a9e5647815652f3656
Chetanya2001 <mathurchetanya1@gmail.com>
2026-03-06 16:14:27 +0530

    ns

--- controller/car.controller.js
+++ controller/car.controller.js
@@ -1136,7 +1136,6 @@ exports.getCarsByHostId = async (req, res) => {
         .status(404)
         .json({ message: "No visible cars found for this host" });
     }
-    console.log(cars.model.name);
 
     const formattedCars = cars.map((car) => ({
       id: car.id,
